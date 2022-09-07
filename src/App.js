d946ed2267b05f220c0e36feefee354fc0452a83
Nghia-ho <nguyenhoannghia.work@gmail.com>
2022-09-07 10:48:01 +0700

    first woo hoo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import "Your code here";
+import { formatTime } from "./formatTime";
 import useTimer from "./useTimer";
 
 function App() {
   const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
-
   return (
     <div className="App container">
-      <h1>Coder Timer</h1>
+      <h1>Coder </h1>
+      <h1>Time </h1>
       <div className="timer__wrapper">
         <div className="timer__display">
-          <p>{"Your code here"}</p>
+          <p>{formatTime(time)}</p>
         </div>
         <div className="button__wrapper">
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={stopTimer}>
             Stop
           </button>
-          <button
-            className="button"
-            ref={"Your code here"}
-            onClick={"Your code here"}
-          >
+          <button className="button" ref={active} onClick={startTimer}>
             Start
           </button>
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={resetTimer}>
             Reset
           </button>
         </div>
